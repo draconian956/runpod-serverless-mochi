c802b038a48ad0191e30e5c88eabe0a26c27a84c
Quantum Ash <quantumash@example.com>
2024-12-13 09:15:30 +0800

    Finish testing mochi.

--- src/start.sh
+++ src/start.sh
@@ -6,4 +6,4 @@ echo "Starting ComfyUI API"
 python3 /app/ComfyUI/main.py --listen "0.0.0.0" --front-end-version "Comfy-Org/ComfyUI_frontend@latest" &
 
 echo "Starting RunPod Handler"
-python3 -u /app/rp_handler.py
+python3 -u "/app/rp_handler.py"
